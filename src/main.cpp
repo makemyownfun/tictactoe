5bd230daf7741cb30617faf451d7aea4b03241b3
Daniel Schmidt <makemyownfun@gmail.com>
2018-02-03 19:38:55 -0800

    Sketched out game loop using comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 
 
 int main(int argc, char* arv[]) {
-    std::cout << "hello tictactoe" << std::endl;
+    bool gameOver = false;
+    while(!gameOver) {
+        // GET NEXT MOVE FROM PLAYER OR COMPUTER
+
+        // ADD THE MOVE TO THE BOARD
+
+        // UPDATE VISUAL STATE OF THE BOARD
+
+        // CHECK FOR A WIN CONDITION
+
+    }
     return 0;
 }
